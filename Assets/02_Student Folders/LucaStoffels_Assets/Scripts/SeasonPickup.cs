3a47b8e857ba94b3c68006316fe391fef7745d85
LucaStoffels <32015566+LucaStoffels@users.noreply.github.com>
2020-09-30 11:41:21 +0200

    Setup Branch
    Imported first version of level art and setup basic lighting, player controll and Gamemanagement

---
+++ Assets/02_Student Folders/LucaStoffels_Assets/Scripts/SeasonPickup.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeasonPickup : MonoBehaviour
+{
+    [Header("Parameters")]
+    [Tooltip("Amount of health to heal on pickup")]
+    public float healAmount;
+
+    Pickup m_Pickup;
+
+    void Start()
+    {
+        m_Pickup = GetComponent<Pickup>();
+        DebugUtility.HandleErrorIfNullGetComponent<Pickup, HealthPickup>(m_Pickup, this, gameObject);
+
+        // Subscribe to pickup action
+        m_Pickup.onPick += OnPicked;
+    }
+
+    void OnPicked(PlayerCharacterController player)
+    {
+        GameObject[] allObjects = UnityEngine.Object.FindObjectsOfType<GameObject>();
+        foreach (GameObject go in allObjects)
+        {
+            Season season = go.GetComponent<Season>();
+            if (season)
+            {
+                season.nextSeason();
+            }
+        }
+
+        m_Pickup.PlayPickupFeedback();
+        Destroy(gameObject);
+    }
+}
