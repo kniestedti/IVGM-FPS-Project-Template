3a27d114a4781a5bdc59b3529bdd9f7735ea7079
roosWe <rooswensveen@hotmail.com>
2021-10-20 17:19:47 +0200

    Commit
    -

--- Assets/02_Student Folders/RoosWensveen_Assets/Scripts/ShootLollipop.cs
+++ Assets/02_Student Folders/RoosWensveen_Assets/Scripts/ShootLollipop.cs
@@ -3,9 +3,11 @@ using System.Collections;
 
 public class ShootLollipop : MonoBehaviour {
 
+    //public LollipopCounter ctr;
 
     public void OnMouseDown()
     {
         Destroy(gameObject);
+        //ctr.counter--;
     }
 }
