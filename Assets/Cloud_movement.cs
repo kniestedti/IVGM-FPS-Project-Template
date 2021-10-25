96226bb5c9e353db868d489e405b9d94c94b3cb5
Blossom1239 <91205565+Blossom1239@users.noreply.github.com>
2021-10-25 15:33:22 +0200

    Cloud
    I hope these clouds work

--- Assets/Cloud_movement.cs
+++ Assets/Cloud_movement.cs
@@ -1,30 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class BalloonScript : MonoBehaviour
+ 
+public class CloudMovement : MonoBehaviour
 {
    [Range(-1f, 1f)]
    public float horizontalSpeed;
-
+ 
     [Range(0, 5)]
     public float verticalSpeed;
-
+ 
     [Range(0, 2)]
     public float amplitude;
-
+ 
     public Vector3 originalPos;
-
+ 
     public Vector3 tempPosition;
-
-
+ 
+ 
     // Start is called before the first frame update
     void Start()
     {
         tempPosition = originalPos = transform.position;
-
+ 
     }
-
+ 
     // Update is called once per frame
     void FixedUpdate()
     {
@@ -32,6 +32,6 @@ public class BalloonScript : MonoBehaviour
         tempPosition.x += horizontalSpeed * Time.fixedDeltaTime;
         tempPosition.y += Mathf.Sin(Time.realtimeSinceStartup * verticalSpeed) * amplitude;
         transform.position = tempPosition;
-
+ 
     }
 }
